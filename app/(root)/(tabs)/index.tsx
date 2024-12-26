b6a8c810a6195a5050f3f1cf20ead924e01bdbf7
Aimad MAJDOU <aimad.majdou@gmail.com>
2024-12-26 01:47:47 +0100

    Fonts, Images, Colors config

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,24 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-3xl my-10 font-rubik">
+        Welcome to Tigmiwin
+      </Text>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href={{ pathname: "/properties/[id]", params: { id: 1 } }}>
+        Property
+      </Link>
+    </View>
+  );
+}
